a3742e91563ed67248a571e60ce21a7400d7c323
alexia pratensi <alexiapratensi@hotmail.fr>
2023-03-04 22:43:58 +0100

    list plant component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BorderCardDirective } from './border-card.directive';
+import { ListPlantComponent } from './list-plant/list-plant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective
+    BorderCardDirective,
+    ListPlantComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/list-plant/list-plant.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-plant',
+  templateUrl: './list-plant.component.html',
+  styles: [
+  ]
+})
+export class ListPlantComponent {
+
+}
